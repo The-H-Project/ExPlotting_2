5177e01051b04c269711428a1358c7f0b5e7605d
Harris Lam <harris.lam@gmail.com>
2015-11-11 18:47:50 -0500

    first commit

---
+++ plot1.R
@@ -0,0 +1,8 @@
+# Course Project 2 Plot 1
+#
+# Load packages.
+library(data.table)
+#
+# Read the data files, and convert them to data tables.
+NEIdt <- data.table(readRDS('../summarySCC_PM25.rds'))
+SCCdt <- data.table(readRDS('../Source_Classification_Code.rds'))
