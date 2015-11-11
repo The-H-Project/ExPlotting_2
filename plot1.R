4efd68bf8a44453635c2e3edec80ec3d7f5ff91c
Harris Lam <harris.lam@gmail.com>
2015-11-11 18:52:38 -0500

    This is a test of git

--- plot1.R
+++ plot1.R
@@ -6,3 +6,5 @@ library(data.table)
 # Read the data files, and convert them to data tables.
 NEIdt <- data.table(readRDS('../summarySCC_PM25.rds'))
 SCCdt <- data.table(readRDS('../Source_Classification_Code.rds'))
+
+# This is a test of git.
